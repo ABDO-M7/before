dc0bd5ef559974019ad38b679530a0508d0c8018
Abdelrahman Mostafa <abdomostafa751761c@gmail.com>
2026-04-06 16:59:06 +0200

    fix(hotfix): restore missing icons and share imports to resolve page crash

--- src/components/PagesComponent/SingleBlog/SingleBlog.jsx
+++ src/components/PagesComponent/SingleBlog/SingleBlog.jsx
@@ -18,14 +18,14 @@ import { setBreadcrumbPath } from "@/redux/reuducer/breadCrumbSlice"
 import { CurrentLanguageData } from "@/redux/reuducer/languageSlice"
 // Ali has commented it cause it is not used or not have to import it - Redundant import, already imported globally
 import toast from "react-hot-toast"
+import { FacebookShareButton, TwitterShareButton, WhatsappShareButton } from "react-share"
+import { RiTwitterXLine } from "react-icons/ri"
+import { BiLink, BiLogoFacebook, BiLogoWhatsapp } from "react-icons/bi"
 // import parse, { domToReact } from 'html-react-parser';
 import Link from "next/link"
 import React from "react"
 import ProductCard from "@/components/Cards/ProductCard"
 import { userSignUpData } from "@/redux/reuducer/authSlice"
-import dynamic from 'next/dynamic'
-
-const BlogSocialShare = dynamic(() => import('./BlogSocialShare'), { ssr: false })
 // Ali has commented it cause it is not used or not have to import it - Redundant import, already imported globally
 // import { toggleLoginModal } from "@/redux/reuducer/globalStateSlice"
 import { Swiper, SwiperSlide } from 'swiper/react';
